ed4eec7aae6d1ae53089cc72481497d9a293fbc5
pigpigpang <1326821869@qq.com>
2021-05-16 23:17:00 +0800

    修改为前后端分离项目

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"ll1_analyse/ll1"
+)
+
+type Data struct {
+	Input string `json:"input"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/analyse", func(c *gin.Context) {
+
+		var parsedBody Data
+		if err := c.BindJSON(&parsedBody); err != nil {
+			fmt.Println(err)
+			fmt.Println(parsedBody)
+			c.JSON(400, gin.H{"message": "参数错误 非可识别Json"})
+			return
+		}
+		input := parsedBody.Input
+		firstmap, followmap, table, b, yesset, noset := ll1.Analyse(input)
+		if b {
+			c.JSON(200, gin.H{
+				"message": "不是ll(1)文法，有冲突",
+				"first":   firstmap,
+				"follow":  followmap,
+				"table":   table,
+				"yesset":  yesset,
+				"noset":   noset,
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"message": "是ll(1)文法",
+				"first":   firstmap,
+				"follow":  followmap,
+				"table":   table,
+				"yesset":  yesset,
+				"noset":   noset,
+			})
+		}
+
+	})
+	r.Run(":5433") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
